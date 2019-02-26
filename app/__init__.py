fa3436c2b754e5c81bd08b7c664e11714e3cd9d2
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2019-02-26 11:59:35 +0200

    add test and shell commanda in manage.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
-# from flask import Flask
-# from flask_bootstrap import Bootstrap
-# from config import config_options
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
 # from flask_sqlalchemy import SQLAlchemy
 # from flask_login import LoginManager
 # from flask_uploads import UploadSet,configure_uploads,IMAGES
@@ -8,7 +8,7 @@
 # from flask_simplemde import SimpleMDE
 
 
-# bootstrap = Bootstrap()
+bootstrap = Bootstrap()
 # db = SQLAlchemy()
 # login_manager = LoginManager()
 # login_manager.session_protection = 'strong'
@@ -17,14 +17,14 @@
 # mail = Mail()
 # simple = SimpleMDE()
 
-# def create_app(config_name):
-#     app = Flask(__name__)
+def create_app(config_name):
+    app = Flask(__name__)
 
 #     # Creating the app configurations
-#     app.config.from_object(config_options[config_name])
+    app.config.from_object(config_options[config_name])
 
 #     # Initializing flask extensions
-#     bootstrap.init_app(app)
+    bootstrap.init_app(app)
 #     db.init_app(app)
 #     login_manager.init_app(app)
 #     mail.init_app(app)
@@ -47,4 +47,4 @@
 #     from .auth import auth as auth_blueprint
 #     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
-#     return app
+    return app
---
+++ start.sh
@@ -0,0 +1,6 @@
+export SECRET_KEY=1234567890
+export MAIL_USERNAME=bellaxbx1109
+export MAIL_PASSWORD=********
+
+
+python3.6 manage.py server
